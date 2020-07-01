66129d446e12d12aff7a0b7dbd4900693b28e546
Leonardo Benitez <benitez.leonardo06@gmail.com>
2020-06-30 22:28:09 -0500

    delete config

--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-export default{
-    jwtSecret: 'api-gazelook_2020',
-    amazon: {
-        accessKeyId: "AKIATSUIFWIDWBLBRSDC",
-        secretAccessKey: "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"
-    }
-}
